e7b6ba681b52813b37f0a844bf58f64234303464
chimezie <chimezie@d0ad5f6e-b329-0410-b51c-492c9c4f233d>
2009-02-02 06:05:30 +0000

    - fixed handling of derived predicates at the head of adorned rules w/out any bound arguments - fixed handling of predicate occurences in sip graphs - added algorithm for finding full sips in a depth first fashion

--- trunk/fuxi/lib/Rete/SidewaysInformationPassing.py
+++ trunk/fuxi/lib/Rete/SidewaysInformationPassing.py
@@ -15,7 +15,7 @@ from rdflib.Collection import Collection
 from testMagic import *
 from cStringIO import StringIO
 from pprint import pprint;
-from rdflib import Namespace
+from rdflib import Namespace, Variable, BNode
 
 MAGIC = Namespace('http://doi.acm.org/10.1145/28659.28689#')
 
@@ -57,41 +57,95 @@ def CollectSIPArcVars(left,right):
     """docstring for CollectSIPArcVars"""
     if isinstance(left,list):
         return set(reduce(lambda x,y:x+y,
-                          [GetArgs(t) for t in left])).intersection(GetArgs(right))
+                          [GetArgs(t,secondOrder=True) for t in left])).intersection(GetArgs(right,secondOrder=True))
     else:
-        return set(GetArgs(left)).intersection(GetArgs(right))
+        return set(GetArgs(left,secondOrder=True)).intersection(GetArgs(right,secondOrder=True))
         
+def SetOp(term,value):
+    if isinstance(term,N3Builtin):
+        term.uri=value
+    elif isinstance(term,Uniterm):
+        if term.op == RDF.type:
+            term.arg[-1]=value
+        else:
+            term.op=value
+    else:
+        raise term        
+                    
 def GetOp(term):
     if isinstance(term,N3Builtin):
         return term.uri
     elif isinstance(term,Uniterm):
         return term.op == RDF.type and term.arg[-1] or term.op
     else:
-        print term
         raise term        
         
-def GetArgs(term):
+def GetArgs(term,secondOrder=False):
     if isinstance(term,N3Builtin):
-        return term.argument
+        return [term.argument,term.result]
     elif isinstance(term,Uniterm):
-        return term.arg
+        args=[]
+        if term.op == RDF.type:
+            if secondOrder and isinstance(term.arg[-1],(Variable, BNode)):
+                args.extend(term.arg)
+            else:
+                args.append(term.arg[0])
+        elif isinstance(term.op,(Variable, BNode)):
+            arg.append(term.op)
+            arg.extend(term.arg)
+        else:
+            args.extend(term.arg)
+        return args
     else:
         raise term        
         
-def IncomingSIPArcs(sip,pred):
+def IncomingSIPArcs(sip,predOcc):
     """docstring for IncomingSIPArcs"""
-    for s,p,o in [(s1,p1,o1) for (s1,p1,o1) in sip.triples((None,None,pred)) 
+    for s,p,o in [(s1,p1,o1) for (s1,p1,o1) in sip.triples((None,None,predOcc)) 
         if (p1,RDF.type,MAGIC.SipArc) in sip]:
         yield Collection(sip,s),Collection(sip,first(sip.objects(p,MAGIC.bindings)))
         
 def validSip(sipGraph):
     if not len(sipGraph): return False
-    for bindingCol in sipGraph.query("SELECT ?arc { [ a m:BoundHeadPredicate; ?arc ?pred ]. ?arc m:bindings [ rdf:first ?val ] }",
-                                     initNs={'m':MAGIC}):
-        return True
-    return False
+    for arc in sipGraph.query(
+         "SELECT ?arc { ?arc m:bindings ?bindings OPTIONAL { ?bindings rdf:first ?val } FILTER(!BOUND(?val)) }",
+         initNs={'m':MAGIC}):
+        return False
+    return True
+
+def getOccurrenceId(uniterm,lookup={}):
+    pO = URIRef(GetOp(uniterm)+'_'+'_'.join(GetArgs(uniterm)))
+    lookup[pO]=GetOp(uniterm)
+    return pO
         
-def BuildNaturalSIP(clause,derivedPreds=None):
+def findFullSip((rt,vars),right):
+    if not vars:
+        if len(rt)==1:
+            vars=GetArgs(rt[0],secondOrder=True)
+        else:
+            vars=reduce(lambda l,r: [i for i in GetArgs(l,secondOrder=True)+GetArgs(r,secondOrder=True) 
+                                                if isinstance(i,(Variable,BNode))],rt)
+    if len(right)==1:
+        if set(GetArgs(right[0],secondOrder=True)).intersection(vars):#len(dq)==1:
+            #Valid End of recursion, return full SIP order
+            yield rt+right  
+    else: 
+        #for every possible combination of left and right, trigger recursive call
+        for item in right:
+            _vars = set([v for v in GetArgs(item,secondOrder=True) if isinstance(v,(Variable,BNode))])
+            _inVars = set([v for v in vars])
+            if _vars.intersection(vars):
+                #There is an incoming arc, continue processing inductively on
+                #the rest of right
+                _inVars.update(_vars.difference(vars))
+                for sipOrder in findFullSip((rt+[item],_inVars),
+                                            [i for i in right if i != item]):
+                    yield sipOrder
+                    
+class InvalidSIPException(Exception):
+    def __init__(self,msg): Exception.__init__(msg)                
+        
+def BuildNaturalSIP(clause,derivedPreds,adornedHead):
     """
     Natural SIP:
     
@@ -115,46 +169,46 @@ def BuildNaturalSIP(clause,derivedPreds=None):
     >>> list(sip.query('SELECT ?q {  ?prop a magic:SipArc . [] ?prop ?q . }',initNs={u'magic':MAGIC}))
     [rdflib.URIRef('http://doi.acm.org/10.1145/28659.28689#sg'), rdflib.URIRef('http://doi.acm.org/10.1145/28659.28689#sg')]
     """
-    from FuXi.Rete.Util import permu
+    from FuXi.Rete.Magic import AdornedUniTerm
+    occurLookup={}
+    boundHead=isinstance(adornedHead,AdornedUniTerm) and 'b' in adornedHead.adornment
+    if not boundHead:
+        pass
     assert isinstance(clause.head,Uniterm),"Only one literal in the head!"
     def collectSip(left,right):
         if isinstance(left,list):
             vars=CollectSIPArcVars(left,right)
             leftList=Collection(sipGraph,None)
-            assert len(left)==len(set(left))            
+            left=list(set(left))            
             [leftList.append(i) for i in [GetOp(ii) for ii in left]]
             left.append(right)                        
-            arc=SIPGraphArc (leftList.uri,GetOp(right),vars,sipGraph)
+            arc=SIPGraphArc(leftList.uri,getOccurrenceId(right,occurLookup),vars,sipGraph)
             return left
         else:
             vars=CollectSIPArcVars(left,right)
             ph=GetOp(left)
-            q=GetOp(right)
+            q=getOccurrenceId(right,occurLookup)
             arc=SIPGraphArc(ph,q,vars,sipGraph,headPassing=True)
-            sipGraph.add((ph,RDF.type,MAGIC.BoundHeadPredicate))
+            if boundHead:
+                sipGraph.add((ph,RDF.type,MAGIC.BoundHeadPredicate))
             rt=[left,right]
         return rt
     sipGraph=Graph()  
     if isinstance(clause.body,And):
-        foundSipOrder = False
-        bodyOrders = permu(clause.body.formulae)
-        while not foundSipOrder:
-            try:
-                bodyOrder=bodyOrders.next()
-                reduce(collectSip,
-                       itertools.chain(
-                              iterCondition(clause.head),
-                              iterCondition(And(bodyOrder))))        
-                if not validSip(sipGraph):
-                    sipGraph.remove((None,None,None))
-                else:
-                    foundSipOrder=True
-                    sipGraph.sipOrder = And(bodyOrder)
-            except StopIteration:
-                raise Exception("Couldn't find a valid SIP for %s"%clause)
+        bodyOrder=first(findFullSip(([clause.head],None), clause.body))
+        assert bodyOrder,"Couldn't find a valid SIP for %s"%clause
+        beginingIndex = boundHead and 1 or 2
+        bodyOrder=bodyOrder[beginingIndex:]
+        sipGraph.sipOrder = And(bodyOrder)
+        reduce(collectSip,
+               boundHead and itertools.chain(
+                                     iterCondition(clause.head),
+                                     iterCondition(And(bodyOrder))) or iterCondition(And(bodyOrder)))
+        #assert validSip(sipGraph),sipGraph.serialize(format='n3')
     else:
-        reduce(collectSip,itertools.chain(iterCondition(clause.head),
-                                          iterCondition(clause.body)))
+        if boundHead:
+            reduce(collectSip,itertools.chain(iterCondition(clause.head),
+                                              iterCondition(clause.body)))
         sipGraph.sipOrder = clause.body        
     if derivedPreds:
         # We therefore generalize our notation to allow
@@ -165,7 +219,7 @@ def BuildNaturalSIP(clause,derivedPreds=None):
         for N,prop,q in sipGraph.query(
             'SELECT ?N ?prop ?q {  ?prop a magic:SipArc . ?N ?prop ?q . }',
             initNs={u'magic':MAGIC}):
-            if q not in derivedPreds:
+            if occurLookup[q] not in derivedPreds:
                 arcsToRemove.extend([(N,prop,q),(prop,None,None)])
                 collectionsToClear.append(Collection(sipGraph,N))
                 #clear bindings collection as well

a4b74a919968e23572eac9d91357d41c6237a917
chimezie <chimezie@d0ad5f6e-b329-0410-b51c-492c9c4f233d>
2010-07-23 22:32:24 +0000

    removed references to BisonGen parser

--- trunk/layercake-python/rdflib/sparql/bison/Processor.py
+++ trunk/layercake-python/rdflib/sparql/bison/Processor.py
@@ -1,20 +1,5 @@
 from rdflib import sparql
 
-def CreateSPARQLParser(debug=False):
-    import SPARQLParserc as SPARQLParser
-    return SPARQLParser.new(debug=debug)    
-
-def Parse(query,debug = False):    
-    p = CreateSPARQLParser(debug)
-    if debug:
-        try:
-           p.debug_mode(1)
-        except:
-            p.debug = 1    
-    if not isinstance(query, unicode):
-        query = unicode(query,'utf-8')
-    return p.parse(query)
-
 class Processor(sparql.Processor):
 
     def __init__(self, graph):
@@ -28,18 +13,16 @@ class Processor(sparql.Processor):
               PARSE_DEBUG=False,
               dataSetBase=None,
               extensionFunctions={},
-              USE_PYPARSING=False,
+              USE_PYPARSING=True,
               parsedQuery=None):
         from rdflib import RDFS, RDF, OWL
         initNs.update({u'rdfs':RDFS.RDFSNS,u'owl':OWL.OWLNS,u'rdf':RDF.RDFNS}) 
         from rdflib.sparql.bison.Query import Query, Prolog
         assert isinstance(queryString, basestring),"%s must be a string"%queryString
         if parsedQuery is None:
-            if USE_PYPARSING:
-                import rdflib.sparql.parser
-                parsedQuery = sparql.parser.parse(queryString)
-            else:
-                parsedQuery = Parse(queryString, PARSE_DEBUG)
+            assert USE_PYPARSING,'C-based BisonGen SPARQL parser has been removed'
+            import rdflib.sparql.parser
+            parsedQuery = sparql.parser.parse(queryString)
         if not parsedQuery.prolog:
                 parsedQuery.prolog = Prolog(None, [])
                 parsedQuery.prolog.prefixBindings.update(initNs)
